665d2d18d7f9084da92b70c413887f394e9e1e1a
gianmarco-todesco <gianmarco.todesco@gmail.com>
2020-04-08 14:39:21 +0200

    .

--- test1.js
+++ test1.js
@@ -3,7 +3,6 @@
 const width = 800;
 const height = width * 0.7;
 let projection; 
-let plane_path;
 let svg;
 
 let geopath;
@@ -31,37 +30,45 @@ let label
 const covidDataUrl = "covid-data.json";
 const worldMapUrl = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson";
 
+// formatter for the current date
 let dateFormatter = new Intl.DateTimeFormat('en', { 
     year: 'numeric', 
     month: 'short', 
     day: '2-digit' 
 }) 
+
+// color scale for PoU index
 const colorScale = d3.scaleLinear().domain([0,100]).range(["#ede", "#62e"])
 
 
-
-
+// initialize the page and fetch needed data
 function initialize() {
     
+    // we use Mercator projection
     projection = d3.geoMercator()
         .scale(width / 2 / Math.PI)
         //.scale(100)
         .translate([width / 2, height *0.7]);
-    plane_path = d3.geoPath().projection(projection);
+
+    // create the SVG element
     svg = d3.select("#container").append("svg")
         .attr("width", width)
         .attr("height", height)
         .attr("class", "map");
     
+    // geopath transforms GeoJson feature into SVG path 
     geopath = d3.geoPath().projection(projection);
 
+    // the label shows the current date
     label = document.getElementById('t')
     label.innerHTML = "Loading data (be patient)..."
 
+    // the slider control the current date
     slider = document.getElementById('slider')
     slider.value = 100
     slider.oninput = onSliderChanged
 
+    // create the tooltip
     tooltip = d3.select("body")
         .append("div")
         .attr("id", "mytooltip")
@@ -70,54 +77,59 @@ function initialize() {
         .style("visibility", "hidden")
         .text("tooltip");
 
+    // fetch all the data
     Promise.all([
         fetch(covidDataUrl).then(d=>d.json()).then(processCovidData),
         fetch(worldMapUrl).then(d=>d.json()).then(processWorldMap),
-        fetch("faostat.json").then(d=>d.json()).then(d=>faoRawData = d)
+        fetch("faostat.json").then(d=>d.json()).then(d=>{
+            faoRawData = d
+            faoRawData.forEach(r => faoValues[r.code] = r.value) 
+        })
     ]).then(()=>{
-        integrateFaoData()
         buildMap()
         label.innerHTML = dateFormatter.format(currentDate)
     })
-
-/*
-
-    // load and display the World
-    d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson", 
-        function(error, data) {
-            data.features.forEach(d => {
-                d.centroid = projection(d3.geoCentroid(d));                
-            });
-    });
-    */
 }
 
 window.onload = initialize
 
-
+// process covid data
+// create a dictonary of dictionaries: date => countrycode => { cases: ..., deaths: ...}
 function processCovidData(data) {
     console.log("processing COVID-19 data")
     covidRawData = data
     data.records.forEach(rec => {
-        
+        // build the date
         let date = new Date(rec.month+"/"+rec.day+"/"+rec.year)
+
+        // get or create covidData[date] 
         let item_t = covidData[date] 
         if(item_t === undefined) item_t = covidData[date] = {}
+
+        // keep track of seen country codes. 
+        // Updates nameByCountryCode[] and countryCodeByName
         let countryCode =  rec.countryterritoryCode
         if(nameByCountryCode[countryCode]===undefined) {
             countryCodes.push(countryCode)
             nameByCountryCode[countryCode] = rec.countriesAndTerritories
             countryCodeByName[rec.countriesAndTerritories] = countryCode
         }
+
+        // get or create covidData[date][countryCode]
         let item_c = item_t[countryCode]
         if(item_c === undefined) item_c = item_t[countryCode] = {}
+
+        // assign data (0 if they are undefined)
         item_c.cases = rec.cases|0
         item_c.deaths = rec.deaths|0
     })
 
+    // collect all the used dates and sort them
     for(let t in covidData) dates.push(new Date(t))    
     dates.sort((a,b) => a.getTime()-b.getTime())
 
+    // fill gaps in the data
+    /*
     let oldValues = {}
     dates.forEach(d => {
         let rec = covidData[d]
@@ -126,21 +138,19 @@ function processCovidData(data) {
             else oldValues[c] = rec[c]
         })
     })
+    */
 
     currentDate = dates[dates.length-1]
     currentRecord = covidData[currentDate]
 }
 
+// process the map data. compute the centroid for each path
 function processWorldMap(data) {
     console.log("processing world map")
     worldMap = data
     data.features.forEach(d => {
         d.centroid = projection(d3.geoCentroid(d));                
     });
-}
-
-function integrateFaoData() {
-    faoRawData.forEach(d => faoValues[d.code] = d.value) 
 }
 
 
@@ -217,7 +227,7 @@ function showTooltip(countryCode) {
     let fv = faoValues[countryCode]
     
     let content = "<h1>"+nameByCountryCode[countryCode]+"</h1><table>" +
-        "<tr><th>POU</th><td>"+(fv>0 ? fv + "%" : "")+"</td></tr>" + 
+        "<tr><th>PoU</th><td>"+(fv>0 ? fv + "%" : "")+"</td></tr>" + 
         "<tr><th>Cases</th><td>"+(data?data.cases:0)+"</td></tr>" + 
         "<tr><th>Deaths</th><td>"+(data?data.deaths:0)+"</td></tr>" + 
         "</table>";
